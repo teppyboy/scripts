67393808d94d25ada03945a4f796bd82ef5173f2
darkelaina <3550664-tretrauit@users.noreply.gitlab.com>
2021-11-07 16:42:14 +0000

    Update rclone-mount.bash

--- Apps/rclone/rclone-mount.bash
+++ Apps/rclone/rclone-mount.bash
@@ -4,10 +4,13 @@ RCLONE_REMOTE="<remote name>"
 ROOT_HOME_DIR="/mnt/runtime/write/emulated/0/"
 RCLONE_MOUNT_DIR="<mount directory>"
 
+FINAL_DIR="$ROOT_HOME_DIR$RCLONE_MOUNT_DIR"
 if [ "$1" = "unmount" ]; then
-    sudo fusermount -u "$ROOT_HOME_DIR$RCLONE_MOUNT_DIR"
-    echo "$RCLONE_REMOTE should be unmounted now"
+    sudo pkill -f "rclone --vfs-cache-mode writes -v mount \"$RCLONE_REMOTE\": \"$FINAL_DIR\" --gid 9997 --dir-perms 0771 --file-perms 0660 --umask=0 --allow-other"
+    sudo fusermount -u "$FINAL_DIR"
+    echo "$RCLONE_REMOTE should be unmounted now, if fusermount raises Invalid option then that's OK."
 else
-    sudo nohup rclone --vfs-cache-mode writes -v mount $RCLONE_REMOTE: "$ROOT_HOME_DIR$RCLONE_MOUNT_DIR" --gid 9997 --dir-perms 0771 --file-perms 0660 --umask=0 --allow-other &
+    sudo mkdir -p "$FINAL_DIR"
+    sudo nohup rclone --vfs-cache-mode writes -v mount "$RCLONE_REMOTE": "$FINAL_DIR" --gid 9997 --dir-perms 0771 --file-perms 0660 --umask=0 --allow-other &
     echo "$RCLONE_REMOTE should be mounted now, check $RCLONE_MOUNT_DIR"
 fi
