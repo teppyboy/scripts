baddfb65b94c7fff267b8cf32c65f0a89f571fb3
darkelaina <3550664-tretrauit@users.noreply.gitlab.com>
2021-11-07 15:24:17 +0000

    Update setup-rclone-termux.bash

--- Apps/rclone/setup-rclone-termux.bash
+++ Apps/rclone/setup-rclone-termux.bash
@@ -16,6 +16,13 @@ else
 fi
 
 echo "!!! YOU NEED ROOT TO RUN THIS SCRIPT !!!"
+echo "This script was made before I discovered that Termux Rclone can work perfectly too if you mount to /mnt/runtime/write/emulated/0/, using this script may lead to unwanted change in your phone."
+read -p "Do you want to continue (Y/n)? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
 echo "Installing dependencies..."
 pkg install root-repo tsu wget
 echo "Setting up Termux shared storage..."
